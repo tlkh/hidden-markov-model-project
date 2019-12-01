c93bde5063c134dd3dd5efe2e190c7aec4da12d3
Timothy Liu <tlkh.xms@gmail.com>
2019-12-01 14:56:06 +0800

    Updated all the stuff

--- emission.py
+++ emission.py
@@ -1,61 +1,46 @@
 import os
-from utils_2 import *
-# def emmision(x, y, hashmap, Y):
-#     if x in hashmap and y in hashmap[x]:
-#         x_to_y = hashmap[x][y]
-#     elif x in hashmap and y not in hashmap[x]:
-#         x_to_y = 0
-#     else:#new words
-#         x = "UNK"
-#         x_to_y = hashmap[x][y]
-#     if y in Y:
-#         count_y = Y[y]
-#     else:
-#         raise RuntimeError("tag not found")
-#     return x_to_y / count_y
+from utils import *
+    
 
-def emission(x, hashmap_smoothed):
-    y = ""
+def get_mle(x, y, emission_data):
+    hashmap = emission_data["x_hashmap"]
+    tags = emission_data["y_tags"]
+    # check if word exists in vocab
     if x in hashmap.keys():
-
-        x_hash = hashmap_smoothed[x]
-        max_count = max(x_hash.values())
-        for i_y in x_hash.keys():
-            if x_hash[i_y] == max_count:
-                y = i_y
+        x_hash = hashmap[x]
     else:
-        x_hash = hashmap_smoothed["UNK"]
-        max_count = max(x_hash.values())
-        for i_y in x_hash.keys():
-            if x_hash[i_y] == max_count:
-                y = i_y
-    return y
+        x_hash = hashmap["#UNK#"]
+    # count(y)
+    total_emissions = tags[y]
+    # count(y->x)
+    try:
+        x_y_emissions = x_hash[y]
+    except KeyError:
+        x_y_emissions = 0
+    # count(y->x) / count(y)
+    mle = x_y_emissions/total_emissions
+    return mle
 
 
-
-
-
-def convert_to_hash_table(lines, lower=True, replace_number=True):
+def generate_emission_table(lines, lower=False, replace_number=False):
     hashmap = {}
     Y = {}
-
     skipped = []
+    hashmap["#UNK#"] = {}
     for line in lines:
         try:
             x, y = line.split(" ")
-            # x - word
+            # x is the word, y is the POS
             if lower:
                 x = x.lower()
             if replace_number:
                 x = x.replace(",", "")
                 try:
                     float(x)
-                    x = "<NUM>"
+                    x = "#NUM#"
                 except:
                     pass
-            # print("orange         orange          oranges")
             if x in hashmap:
-
                 if y in hashmap[x]:
                     hashmap[x][y] += 1
                 else:
@@ -67,50 +52,10 @@ def convert_to_hash_table(lines, lower=True, replace_number=True):
                 Y[y] += 1
             else:
                 Y[y] = 1
-
-
-
         except Exception as e:
             if line not in skipped:
                 skipped.append(line)
-    # print("Skipped", len(skipped), "lines: ", end="")
-    # print(skipped, "\n")
-    return hashmap, Y
-
-def add_unk(hashmap, k = 3):
-    hashmap["UNK"] = {}
-    for x in hashmap.keys():
-        count_x = sum(hashmap[x].values())
-        if count_x <= k:
-            for y_of_x in hashmap[x]:
-                if y_of_x in hashmap["UNK"]:
-                    hashmap["UNK"][y_of_x] += hashmap[x][y_of_x]
-                else:
-                    hashmap["UNK"][y_of_x] = hashmap[x][y_of_x]
-
-    return hashmap
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--folder', type=str, required=True, help='Path to the Cityscapes gtFine directory.')
-    opt = parser.parse_args()
-    folder = opt.folder
- 
-    # folder = "..\AL\AL"  
-    # folder = "..\EN\EN"  
-    # folder = "..\SG\SG"  
-    # folder = "..\CN\CN"
-    lines = read_file_to_lines(folder + "\\train")
-    hashmap = convert_to_hash_table(lines)[0]
-    hashmap_smoothed = add_unk(hashmap)
-    test = read_file_to_lines(folder + "\dev.in")
-    for i in test:
-        if i == "":
-            continue
-        elif i == ".":
-            y = emission(i, hashmap_smoothed)
-            print(i + " " + y + "\n")
-            continue
-        y = emission(i,hashmap_smoothed)
-        print(i + " " + y)
+    print("Skipped", len(skipped), "lines: ", end="")
+    print(skipped, "\n")
+    return {"x_hashmap": hashmap,
+            "y_tags": Y}
--- utils.py
+++ utils.py
@@ -1,7 +1,28 @@
+import os
+
 def read_file_to_lines(path):
-    lines = [line.rstrip('\n') for line in open(path)]
+    if "AL" in path:
+        lines = [line.rstrip('\n') for line in open(path, encoding='gb18030', errors='ignore')]
+    else:
+        lines = [line.rstrip('\n') for line in open(path, encoding='utf-8')]#, encoding='gb18030', errors='ignore'
     return lines
-    
+
+def add_unk(hashmap, k=3):
+    smoothed_hashmap = {}
+    smoothed_hashmap["#UNK#"] = {}
+    for x in hashmap.keys():
+        count_x = sum(hashmap[x].values())
+        if count_x <= k:
+            for y_of_x in hashmap[x]:
+                if y_of_x in smoothed_hashmap["#UNK#"]:
+                    smoothed_hashmap["#UNK#"][y_of_x] += hashmap[x][y_of_x]
+                else:
+                    smoothed_hashmap["#UNK#"][y_of_x] = hashmap[x][y_of_x]
+        else:
+            smoothed_hashmap[x] = hashmap[x]
+    return smoothed_hashmap
+
+
 def convert_to_train_set(lines, lower=True, replace_number=True):
     X, Y = [], []
     skipped = []
@@ -15,7 +36,7 @@ def convert_to_train_set(lines, lower=True, replace_number=True):
                 x = x.replace(",", "")
                 try:
                     float(x)
-                    x = "<NUM>"
+                    x = "#NUM#"
                 except:
                     pass
             X.append(x.strip())
@@ -25,9 +46,10 @@ def convert_to_train_set(lines, lower=True, replace_number=True):
             if line not in skipped:
                 skipped.append(line)
     print("Skipped", len(skipped), "lines: ", end="")
-    print(skipped, "\n")
+    print(skipped)
     return X, Y
-        
+
+
 def return_vocab(token_list):
     vocab_list = list(set(token_list))
     vocab_list.sort()
